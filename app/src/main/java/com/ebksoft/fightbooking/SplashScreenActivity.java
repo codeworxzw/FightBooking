950428876c1c56c35bde9e2bd0f9b7c2f20aa43a
nhutcm <chauminhnhut90@gmail.com>
2016-01-05 23:41:31 +0700

    Project FightBooking
    Add project

---
+++ app/src/main/java/com/ebksoft/fightbooking/SplashScreenActivity.java
@@ -0,0 +1,52 @@
+package com.ebksoft.fightbooking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by chauminhnhut on 1/5/16.
+ */
+public class SplashScreenActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setImageResource(R.drawable.ic_splash);
+        setContentView(imageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(mContext, HomeActivity.class));
+                finish();
+            }
+        }, 3000);
+
+        loadView();
+        loadData();
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void loadView() {
+
+    }
+}
